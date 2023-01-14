7cf6704f5ffea18f44634eee1fc9b0ad207dcfc2
soyombo <soyombo0@naver.com>
2023-01-14 13:25:40 +0300

    Initial Commit

---
+++ mineScript/mineScriptApp.swift
@@ -0,0 +1,17 @@
+//
+//  mineScriptApp.swift
+//  mineScript
+//
+//  Created by Soyombo Mantaagiin on 14.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct mineScriptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
